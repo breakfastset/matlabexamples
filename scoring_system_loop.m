dca41704e30700ed8b1e91ddc8cb767041acff48
breakfastset <34071068+breakfastset@users.noreply.github.com>
2020-06-07 16:19:33 +0800

    Add while loop examples

---
+++ scoring_system_loop.m
@@ -0,0 +1,30 @@
+DEFAULT_SIZE = 50;
+EXIT_OPTION = -1;
+
+scores=zeros(1, DEFAULT_SIZE);
+
+score = input('Enter a student score (-1 to exit): ');
+studentCount = 0;
+
+while score ~= EXIT_OPTION
+  if score > 100 || score < 0
+    fprintf('Please enter a valid score (0 to 100)!');
+  else
+    studentCount = studentCount + 1;
+    scores(studentCount) = score;
+  end
+  score = input('Enter a student score (-1 to exit): ');
+end
+
+if studentCount == 0
+  fprintf('No student scores entered!\n');
+else
+  if studentCount < DEFAULT_SIZE
+    scores = scores(1:studentCount);
+  end
+  fprintf('Average score: %d\n', mean(scores));
+  fprintf('Median score: %d\n', median(scores));
+  fprintf('Highest score: %d\n', max(scores));
+  fprintf('Lowest score: %d\n', min(scores));
+  fprintf('Standard deviation: %d\n', std(scores));
+end
---
+++ while_loop_ex.m
@@ -0,0 +1,7 @@
+
+
+count = 1;
+while count <= 5
+  fprintf('Count is %d\n', count);
+  count = count + 1;
+end
