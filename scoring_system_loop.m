a7b2ed79557bfa187280eb4fe9a85d1e36421ebd
breakfastset <34071068+breakfastset@users.noreply.github.com>
2020-06-07 16:37:54 +0800

    Update of user input checking for empty entry

--- scoring_system_loop.m
+++ scoring_system_loop.m
@@ -1,19 +1,27 @@
 DEFAULT_SIZE = 50;
 EXIT_OPTION = -1;
+INVALID_SCORE = -999;
 
 scores=zeros(1, DEFAULT_SIZE);
 
 score = input('Enter a student score (-1 to exit): ');
 studentCount = 0;
 
+if length(score) == 0
+  score = INVALID_SCORE;
+end
+
 while score ~= EXIT_OPTION
   if score > 100 || score < 0
-    fprintf('Please enter a valid score (0 to 100)!');
+    fprintf('Please enter a valid score (0 to 100)!\n');
   else
     studentCount = studentCount + 1;
     scores(studentCount) = score;
   end
   score = input('Enter a student score (-1 to exit): ');
+  if length(score) == 0
+    score = INVALID_SCORE;
+  end
 end
 
 if studentCount == 0
